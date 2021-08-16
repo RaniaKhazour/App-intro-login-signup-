a7d9cce228aec236d11fb45865b7170a7bdf8b43
Rania <71328206+RaniaKhazour@users.noreply.github.com>
2021-08-16 11:44:39 +0200

    Add files via upload

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -37,39 +37,45 @@ const SignUp = () => {
                     </figure>
                 </Link>
 
-                    <h1 className="text-2xl lg:text-5xl font-bold text-gray-700 lg:mb-6 mb-2 drop-shadow-xl">Tilmelde</h1>
-                    
+                <h1 className="text-2xl lg:text-5xl font-bold text-gray-700 lg:mb-6 mb-2 drop-shadow-xl">Tilmelde</h1>
+
+                <div className="max-w-sm w-full h-14 mt-3 rounded-2xl bg-gray-100 flex justify-around py-1.5 shadow-sm flex-row">
                     <Input 
                         icon="fa-envelope"
                         type="text"
                         placeholder="Email"
                         nameInput="email"
                     />
-
+                </div>
+                
+                <div className="max-w-sm w-full h-14 mt-3 rounded-2xl bg-gray-100 flex justify-around py-1.5 shadow-sm flex-row">
                     <Input 
                         icon="fa-lock"
                         type="password"
                         placeholder="Adgangskode"
                         nameInput="psw"
                     />
+                </div>
 
+                <div className="max-w-sm w-full h-14 mt-3 rounded-2xl bg-gray-100 flex justify-around py-1.5 shadow-sm flex-row">
                     <Input 
                         icon="fa-check"
                         type="password"
                         placeholder="Bekræft adgangskode"
                         nameInput="psw-check"
                     />
+                </div>
 
-                    <div className="max-w-sm w-full h-14 mt-3 ml-5 rounded-2xl grid grid-cols-10 py-1.5">
-                        <input className="col-span-1 text-center mt-2" type="checkbox" id="vehicle1" name="vehicle1" value="Bike" required/>
-                        <label className="col-span-9 text-gray-500" for="vehicle1">Jeg accepterer Handelsvilkårene og <br/> Persondatapolitikken</label>
-                    </div>
-                    
-                    <button className="rounded-xl bg-green-700 text-white  px-10 py-3.5 ml-2 shadow-lg filter drop-shadow-sm mt-8 transition duration-500 hover:bg-green-600" type="submit" value="login">Tilmeld dig</button>
+                <div className="max-w-sm w-full h-14 mt-3 ml-5 rounded-2xl grid grid-cols-10 py-1.5">
+                    <input className="col-span-1 text-center mt-2" type="checkbox" id="vehicle1" name="vehicle1" value="Bike" required/>
+                    <label className="col-span-9 text-gray-500" for="vehicle1">Jeg accepterer Handelsvilkårene og <br/> Persondatapolitikken</label>
+                </div>
+                
+                <button className="rounded-xl bg-green-700 text-white  px-10 py-3.5 ml-2 shadow-lg filter drop-shadow-sm mt-8 transition duration-500 hover:bg-green-600" type="submit" value="login">Tilmeld dig</button>
 
-                    <div className="lg:hidden mt-10 border-t-2 border-gray-300">
-                        <p className="text-gray-600 mt-4">En af ​​Kanpla?<Link href="/login"><span className="text-green-700 ml-1">Log ind</span></Link></p>
-                    </div>
+                <div className="lg:hidden mt-10 border-t-2 border-gray-300">
+                    <p className="text-gray-600 mt-4">En af ​​Kanpla?<Link href="/login"><span className="text-green-700 ml-1">Log ind</span></Link></p>
+                </div>
             </animated.form>
 
             <animated.div style={slideToRight} className="bg-green-800 flex-1 h-screen hidden lg:block shape-r">
