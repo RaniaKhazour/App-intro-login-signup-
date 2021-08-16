a4117c7fe1fbd50a78c48154205d9ae320cfadf3
Rania <71328206+RaniaKhazour@users.noreply.github.com>
2021-08-16 12:05:43 +0200

    Add files via upload

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default class CustomDocument extends Document {
             <Html lang="da">
                 <Head>
                     <meta name="keywords" content="Kanpla"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"></meta>
                     <title>Kanpla</title>
                     <link rel="shortcut icon" href="/icons/icon_white.png" />
                     <script src="https://kit.fontawesome.com/5f85226895.js" crossorigin="anonymous"></script>
