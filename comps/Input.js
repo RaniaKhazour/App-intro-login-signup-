eaba2cc2463be51618a0c85ebaed77581d793a92
Rania <71328206+RaniaKhazour@users.noreply.github.com>
2021-08-16 10:47:55 +0200

    Add files via upload

--- comps/Input.js
+++ comps/Input.js
@@ -1,6 +1,9 @@
-const Input = ({ type, placeholder, nameInput }) => {
+const Input = ({ icon, type, placeholder, nameInput }) => {
     return (
-        <input className="col-span-8 bg-transparent outline-none border-none font-semibold text-lg text-gray-800" type={type} placeholder={placeholder} name={nameInput} required></input>
+        <div className="max-w-sm w-full h-14 mt-3 rounded-2xl bg-gray-100 flex justify-around py-1.5 shadow-sm flex-row">
+            <i class={`fas ${icon} text-center mt-2 text-gray-500 text-lg flex-1`}></i>
+            <input className="bg-transparent outline-none border-none font-semibold text-lg text-gray-800 flex-4" type={type} placeholder={placeholder} name={nameInput} required></input>
+        </div>
     );
 }
 
